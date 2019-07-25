eb7a6f4ad57497a7459af9ae7b1f81033b46b05d
Cihat Pala <cihat_pala@hotmail.com>
2019-07-25 09:47:11 +0300

    Use to draw information from websites

---
+++ Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace HtmlAgilityPack
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox4 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.listBox5 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.listBox6 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(470, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Haberleri Çek";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 76);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(1013, 82);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Console", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(31, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Başlıklar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Console", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(31, 170);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "İçerikler";
+            this.label2.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(12, 186);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(1013, 82);
+            this.listBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Console", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(31, 285);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Yazarlar";
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(12, 301);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(1013, 82);
+            this.listBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lucida Console", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(31, 397);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Haber Linkleri";
+            // 
+            // listBox4
+            // 
+            this.listBox4.FormattingEnabled = true;
+            this.listBox4.Location = new System.Drawing.Point(12, 413);
+            this.listBox4.Name = "listBox4";
+            this.listBox4.Size = new System.Drawing.Size(1013, 82);
+            this.listBox4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Lucida Console", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(31, 505);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(157, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Fotoğraf Idleri";
+            this.label5.Click += new System.EventHandler(this.Label5_Click);
+            // 
+            // listBox5
+            // 
+            this.listBox5.FormattingEnabled = true;
+            this.listBox5.Location = new System.Drawing.Point(12, 521);
+            this.listBox5.Name = "listBox5";
+            this.listBox5.Size = new System.Drawing.Size(219, 82);
+            this.listBox5.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Lucida Console", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(266, 505);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(177, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Fotoğraf Linkleri";
+            // 
+            // listBox6
+            // 
+            this.listBox6.FormattingEnabled = true;
+            this.listBox6.Location = new System.Drawing.Point(247, 521);
+            this.listBox6.Name = "listBox6";
+            this.listBox6.Size = new System.Drawing.Size(778, 82);
+            this.listBox6.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1037, 638);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.listBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.listBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListBox listBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ListBox listBox6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace HtmlAgilityPack
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        string[] donanimHeader = new string[35];
+        string[] donanimAuthor = new string[35];
+        string[] donanimContent = new string[35];
+        string[] donanimImage = new string[35];
+        string[] donanimHaberLink = new string[35];
+        int[] donanimFotoId = new int[35];
+        public String html;
+        public Uri url;
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 2; i++)
+            {
+                //VeriAlDip(donanimHaberLink[i], "//*[@id='res-112646-1']/span/a", "href", listBox5); //Haber Image
+                //*[@id="kapsulDiv"]/div[1]/div[1]/div[1]/div[6]/div/a
+                VeriAl("https://www.donanimhaber.com/", "//*[@id='kapsulDiv']/div[1]/div[1]/div/div[" + (i + 1) + "]/div/div/a", listBox1); //Haber Header
+                
+                VeriAl("https://www.donanimhaber.com/", "//*[@id='kapsulDiv']/div[1]/div[1]/div/div[" + (i + 1) + "]/div/div/div[2]", listBox2); //Haber Content
+                VeriAl("https://www.donanimhaber.com/", "//*[@id='kapsulDiv']/div[1]/div[1]/div/div[" + (i + 1) + "]/div/div/div[1]", listBox3); //Haber Author
+                VeriAlDip("https://www.donanimhaber.com/", "//*[@id='kapsulDiv']/div[1]/div[1]/div/div[" + (i + 1) + "]/div/div/a", "href", listBox4); //Haber Link
+                donanimHaberLink[i] = listBox4.Items[i].ToString();
+                VeriAlDip("https://www.donanimhaber.com/", "//*[@id='kapsulDiv']/div[1]/div[1]/div[1]/div[" + (i + 1) + "]/div/a", "data-id", listBox5); //Haber Foto id
+                donanimFotoId[i] = Convert.ToInt32(listBox5.Items[i]);
+                VeriAlDip(donanimHaberLink[i], "//*[@id='res-" + donanimFotoId[i] + "-1']/span/a", "href", listBox6); //Haber Image
+                
+                
+                //string s = WebUtility.HtmlDecode("&eacute;"); // Returns é
+
+            }
+            for (int i = 0; i < 2; i++)
+            {
+                donanimHeader[i] = listBox1.Items[i].ToString();
+                donanimHeader[i] = WebUtility.HtmlDecode(donanimHeader[i]);
+                listBox1.Items.Add(donanimHeader[i]);
+
+                donanimContent[i] = listBox2.Items[i].ToString();
+                donanimContent[i] = WebUtility.HtmlDecode(donanimContent[i]);
+                listBox2.Items.Add(donanimContent[i]);
+
+                donanimAuthor[i] = listBox3.Items[i].ToString();
+                donanimAuthor[i] = karakterTemizle(WebUtility.HtmlDecode(donanimAuthor[i]));
+                listBox3.Items.Add(donanimAuthor[i]);
+
+            }
+
+            
+        }
+       
+
+        public string karakterTemizle(string gelen)
+        {
+            string trans = gelen;
+            trans = Regex.Replace(trans, "\r\n", String.Empty);
+            trans = trans.TrimEnd();
+            trans = trans.TrimStart();
+            return trans;
+        }
+
+        public void VeriAl(String Url, String XPath, ListBox cikanSonuc)
+        {
+            try
+            {
+                url = new Uri(Url);
+            }
+            catch (UriFormatException)
+            {
+                if (MessageBox.Show("Hatalı Url", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error) == DialogResult.OK)
+                {
+                    url = new Uri("https://www.donanimhaber.com");
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                if (MessageBox.Show("Boş Argüman", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error) == DialogResult.OK)
+                {
+                    url = new Uri("https://www.donanimhaber.com");
+                }
+            }
+
+            WebClient client = new WebClient();
+            client.Encoding = Encoding.UTF8;
+            try
+            {
+                html = client.DownloadString(url);
+            }
+            catch (WebException)
+            {
+                if (MessageBox.Show("Url indirilemedi!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error) == DialogResult.OK)
+                {
+                    html = client.DownloadString("https://www.donanimhaber.com");
+                }
+            }
+
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(html);
+            try
+            {
+                cikanSonuc.Items.Add(doc.DocumentNode.SelectSingleNode(XPath).InnerText);
+            }
+            catch (NullReferenceException)
+            {
+                if (MessageBox.Show("Hatalı XPath Haber, İçerik, Yazar vs...", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error) == DialogResult.OK)
+                {
+                    cikanSonuc.Items.Add("Hatasız Kul Olmaz");
+                }
+            }
+
+        }
+        public void VeriAlDip(String Url, String XPath, String dip, ListBox cikanSonuc)
+        {
+            try
+            {
+                url = new Uri(Url);
+            }
+            catch (UriFormatException)
+            {
+                if (MessageBox.Show("Hatalı Url", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error) == DialogResult.OK)
+                {
+                    url = new Uri("https://www.donanimhaber.com");
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                if (MessageBox.Show("Boş Argüman", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error) == DialogResult.OK)
+                {
+                    url = new Uri("https://www.donanimhaber.com");
+                }
+            }
+
+            WebClient client = new WebClient();
+            client.Encoding = Encoding.UTF8;
+            try
+            {
+                html = client.DownloadString(url);
+            }
+            catch (WebException)
+            {
+                if (MessageBox.Show("Url indirilemedi!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error) == DialogResult.OK)
+                {
+                    html = client.DownloadString(new Uri("https://www.donanimhaber.com"));
+                }
+            }
+            
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(html);
+            try
+            {
+                if (Url == "https://www.donanimhaber.com/")
+                {
+                    if (dip=="data-id")
+                    {
+                        try
+                        {
+                            cikanSonuc.Items.Add(doc.DocumentNode.SelectSingleNode(XPath).Attributes[dip].Value);
+                        }
+                        catch (Exception)
+                        {
+                            if (MessageBox.Show("Bozuk veri geliyor", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error) == DialogResult.OK)
+                            {
+                                cikanSonuc.Items.Add(11111);
+                            }
+                        }
+                        
+                    }
+                    else
+                    {
+                        cikanSonuc.Items.Add("https://www.donanimhaber.com" + doc.DocumentNode.SelectSingleNode(XPath).Attributes[dip].Value);
+                    }
+                }
+                else if(dip=="href" || Url != "https://www.donanimhaber.com/")
+                {
+                    //cikanSonuc.Items.Add(doc.DocumentNode.SelectSingleNode(XPath).Attributes[dip].Value);
+                    try
+                    {
+                        cikanSonuc.Items.Add(doc.DocumentNode.SelectSingleNode(XPath).Attributes[dip].Value);
+                    }
+                    catch (Exception)
+                    {
+                        if (MessageBox.Show("Bu linkten fotoğraf çekilemiyor!!", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error) == DialogResult.OK)
+                        {
+                            cikanSonuc.Items.Add("https://www.ekol.com/wp-content/uploads/2015/07/ekol-logotype.png");
+                        }
+                    }
+                }
+                
+            }
+            catch (NullReferenceException)
+            {
+                if (MessageBox.Show("Hatalı XPath Foto ve link", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error) == DialogResult.OK)
+                {
+                    cikanSonuc.Items.Add("Hatasız Kul Olmaz");
+                }
+            }
+
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label5_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
